a1848b52239fedce371b0929ae88b95cca1c2526
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2022-11-29 23:37:29 +0300

    Delete try.py

--- 0x16-api_advanced/try.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-data = requests.get('https://www.reddit.com/r/programming/hot/.json', headers={'User-Agent': "subscribers"})
-
-print(data.json().get("data").get('children'))
-n = data.json().get("data").get('children')
-
-print(len(n))
